e1384bc31b61f3aa3f4ac48ac7ee264f45d86977
Oleg Grunski <oleggr578@gmail.com>
2025-02-08 12:32:13 +0100

    add InputForm component for reusable form input handling

---
+++ src/components/InputForm.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="form-group">
+		<label :for="name">{{ label }}</label>
+		<input :style="'margin-bottom: ' + marginBottom" :type="type" class="form-control" :id="name" v-model="$parent[name]"  />
+	</div>
+</template>
+
+<script>
+export default {
+    name: 'InputForm',
+
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+
+        name: {
+            type: String,
+            required: true
+        },
+
+        marginBottom: {
+            type: String,
+            required: false,
+            default: '0px'
+        },
+
+        type: {
+            type: String,
+            required: false,
+            default: 'text'
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+label {
+    font-size: 13px;
+}
+
+.form-group {
+    margin-bottom: 5px;
+}
+</style>
