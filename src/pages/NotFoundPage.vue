dd4fdaeb29d1898cd7228266e204f7f5d5302d79
Oleg Grunski <oleggr578@gmail.com>
2025-02-12 00:00:25 +0100

    enhance NotFoundPage: update navigation to return to last visited location

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -2,13 +2,20 @@
 	<div class="not-found">
 		<h1>404 - Page Not Found</h1>
 		<p>The page you are looking for does not exist.</p>
-		<router-link to="/" class="btn btn-primary">Go Back Home</router-link>
+		<router-link :to="lastLocation" class="btn btn-primary">Go Back</router-link>
 	</div>
 </template>
 
 <script>
+
 export default {
 	name: 'NotFoundPage',
+
+	computed: {
+		lastLocation() {
+			return this.$store.getters.lastLocation
+		}
+	}
 };
 </script>
 
