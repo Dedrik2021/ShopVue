78f87e87c4a9ab492436a0120d44b538834a1cb6
Oleg Grunski <oleggr578@gmail.com>
2025-02-23 02:03:43 +0100

    add order management actions: implement createOrder, getOrderById, updateProfile, and saveShippingAddress functions with error handling

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,6 +9,26 @@ const config = {
 };
 
 export default {
+
+    async createOrder({ commit }, order) {
+        commit('setStatus', 'pending');
+        try {
+            const response = await axios.post(`${url}orders/add/`, order, {
+                headers: {
+                    Authorization: `Bearer ${order.token}`,
+                },
+            });
+            commit('setOrder', response.data);
+            commit('setStatus', 'success');
+            return response.data;
+        } catch (error) {
+            commit('setError', `${error.response.data.detail}. Try again.`);
+            console.error(error);
+        } finally {
+            commit('setStatus', 'success');
+        }
+    },
+
 	async registerUser({ commit }, data) {
 		commit('setStatus', 'pending');
 		try {
@@ -22,6 +42,44 @@ export default {
 			commit('setStatus', 'success');
 		}
 	},
+	async getUserInfo({ commit, state }) {
+		commit('setStatus', 'pending');
+		try {
+			const response = await axios.get(`${url}users/profile/`, {
+				headers: {
+					Authorization: `Bearer ${state.user.token}`,
+				},
+			});
+
+			commit('setUserInfo', response.data);
+			commit('setStatus', 'success');
+		} catch (error) {
+			commit('setError', `${error.response.data}. Try again.`);
+			console.error(error);
+		} finally {
+			commit('setStatus', 'success');
+		}
+	},
+
+	async updateProfile({ commit }, data) {
+		commit('setStatus', 'pending');
+		try {
+			const response = await axios.put(`${url}users/profile/update/`, data, {
+				headers: {
+					Authorization: `Bearer ${data.token}`,
+				},
+			});
+			commit('setUserInfo', response.data);
+            localStorage.setItem('userInfo', JSON.stringify(response.data));
+			commit('setStatus', 'success');
+		} catch (error) {
+			commit('setError', `${error.response.data}. Try again.`);
+			console.error(error);
+		} finally {
+			commit('setStatus', 'success');
+		}
+	},
+
 	async loginUser({ commit }, data) {
 		commit('setStatus', 'pending');
 		try {
@@ -75,6 +133,7 @@ export default {
 		try {
 			const response = await axios.get(`${url}products/${id}/`);
 			commit('addToCart', { product: response.data, qty, username });
+            return response.data
 		} catch (error) {
 			commit('setError', 'An error occurred while adding to cart.');
 			console.error(error);
@@ -82,6 +141,7 @@ export default {
 			commit('setStatus', 'success');
 		}
 	},
+
 	async removeItemFromCart({ commit }, id) {
 		commit('setStatus', 'pending');
 		try {
@@ -112,5 +172,39 @@ export default {
 	},
 	setUser({ commit }, user) {
 		commit('setUser', user);
+	},
+
+    async saveShippingAddress({ commit }, address) {
+        commit('setStatus', 'pending');
+
+        try {
+            await commit('setShippingAddress', address);
+        } catch (error) {
+            commit('setError', `${error.response.data.detail}. Try again.`);
+            console.error(error);
+        } finally {
+            commit('setStatus', 'success');
+        }
+        commit('setStatus', 'success');
+    },
+
+
+	async getOrderById({ commit }, data) {
+		commit('setStatus', 'pending');
+		try {
+			
+			const response = await axios.get(`${url}orders/${data.id}/`, {
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${data.token}`,
+				},
+			});
+			commit('setOrder', response.data);
+		} catch (error) {
+			commit('setError', `${error.response.data.detail}. Go Home Page.`);
+			console.error('error', error.response.data.detail);
+		} finally {
+			commit('setStatus', 'success');
+		}
 	},
 };
